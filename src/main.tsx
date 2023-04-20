7d55e4adac6669f9e873bb63187c518146c7885c
Elton Santos <eltonaxl@hotmail.com>
2023-04-20 01:31:27 -0300

    Routing in react

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,9 @@ import ReactDOM from "react-dom/client";
 
 import "./global.css";
 
-import { Tweet } from "./components/Tweet/Tweet";
 import { Sidebar } from "./components/Sidebar/Sidebar";
-import { Header } from "./components/Header/Header";
-import { Separator } from "./components/Separator/Separator";
-
-const tweets = [
-  "Tweet 1",
-  "Estou fazendo um projeto bem legal que é tentar criar a user interface do Twitter. Basicamente estou tentando fazer o clone da interface. Está ficando irado, estou seguindo o tutorial da Rocketseat, nesse link aqui: https://www.youtube.com/watch?v=99okStmx-7c",
-  "Tweet 3",
-];
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
@@ -20,26 +13,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
       <Sidebar />
 
       <div className="content">
-        <main className="timeline">
-          <Header title="Home" />
-
-          <form className="new-tweet-form">
-            <label htmlFor="tweet">
-              <img
-                src="https://github.com/eltonsantos.png"
-                alt="Elton Santos"
-              />
-              <textarea id="tweet" placeholder="What's happening?" />
-            </label>
-            <button type="submit">Tweet</button>
-          </form>
-
-          <Separator />
-
-          {tweets.map((tweet) => (
-            <Tweet key={tweet} content={tweet} />
-          ))}
-        </main>
+        <RouterProvider router={router} />
       </div>
     </div>
   </React.StrictMode>
---
+++ src/pages/Timeline.tsx
@@ -0,0 +1,31 @@
+import { Header } from "../components/Header/Header";
+import { Separator } from "../components/Separator/Separator";
+import { Tweet } from "../components/Tweet/Tweet";
+import "./Timeline.css";
+
+const tweets = [
+  "Tweet 1",
+  "Estou fazendo um projeto bem legal que é tentar criar a user interface do Twitter. Basicamente estou tentando fazer o clone da interface. Está ficando irado, estou seguindo o tutorial da Rocketseat, nesse link aqui: https://www.youtube.com/watch?v=99okStmx-7c",
+  "Tweet 3",
+];
+
+export function Timeline() {
+  return (
+    <main className="timeline">
+      <Header title="Home" />
+      <form className="new-tweet-form">
+        <label htmlFor="tweet">
+          <img src="https://github.com/eltonsantos.png" alt="Elton Santos" />
+          <textarea id="tweet" placeholder="What's happening?" />
+        </label>
+        <button type="submit">Tweet</button>
+      </form>
+
+      <Separator />
+
+      {tweets.map((tweet) => (
+        <Tweet key={tweet} content={tweet} />
+      ))}
+    </main>
+  );
+}
