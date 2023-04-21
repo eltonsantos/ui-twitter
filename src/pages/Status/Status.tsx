3fdddeb376908fa3ab04409ce48f90cd96a80bf9
Elton Santos <eltonaxl@hotmail.com>
2023-04-21 11:44:02 -0300

    Add new answers

--- src/pages/Status/Status.tsx
+++ src/pages/Status/Status.tsx
@@ -1,12 +1,26 @@
+import { FormEvent, useState } from "react";
 import { Header } from "../../components/Header/Header";
 import { Separator } from "../../components/Separator/Separator";
 import { Tweet } from "../../components/Tweet/Tweet";
 
 import "./Status.css";
 
-const answers = ["Concordo", "Faz sentido", "Legal oh"];
-
 export function Status() {
+  const [newAnswer, setNewAnswer] = useState("");
+  const [answers, setAnswers] = useState([
+    "Concordo",
+    "Faz sentido",
+    "Legal oh",
+  ]);
+
+  function createNewAnswer(event: FormEvent) {
+    event.preventDefault();
+
+    setAnswers([...answers, newAnswer]);
+
+    setNewAnswer("");
+  }
+
   return (
     <main className="status">
       <Header title="Tweet" />
@@ -15,10 +29,17 @@ export function Status() {
 
       <Separator />
 
-      <form className="answer-tweet-form">
+      <form onSubmit={createNewAnswer} className="answer-tweet-form">
         <label htmlFor="tweet">
           <img src="https://github.com/eltonsantos.png" alt="Elton Santos" />
-          <textarea id="tweet" placeholder="Tweet your answer" />
+          <textarea
+            id="tweet"
+            value={newAnswer}
+            placeholder="Tweet your answer"
+            onChange={(event) => {
+              setNewAnswer(event.target.value);
+            }}
+          />
         </label>
         <button type="submit">Answer</button>
       </form>
